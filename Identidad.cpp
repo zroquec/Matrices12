a0ba08718e8b0ad3b31bd77c2173d227035aa240
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 10:06:07 -0500

    Identidad: terminado

--- Identidad.cpp
+++ Identidad.cpp
@@ -20,5 +20,13 @@ int main()
             }
         }
     }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << Id[i][j] << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
