f7a8563ee6b49236acf7efb75f4c9cb2eaf3c199
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 17:48:45 -0500

    sumadiagonal: avance 2

--- sumadiagonal.cpp
+++ sumadiagonal.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
-    
+    int n,sum=0;
+    int matriz[100][100];
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>matriz[i][j];
+        }
+    }
+
     return 0;
 }
