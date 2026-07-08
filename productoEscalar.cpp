2347ff6d4b28720d23295e582c01f3e98fe13a75
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 18:55:19 -0500

    productoEscalar: avance 2

--- productoEscalar.cpp
+++ productoEscalar.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main(){
+    int k, m, n;
+    int A[100][100];
+    int P[100][100];
+    cin>>k;
+    cin>>m;
+    cin>>n;
     
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cin>>A[i][j];
+        }
+    }
     return 0;
 }
