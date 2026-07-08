0790dfc8f50ed48b07a65954bbfc95b19b22c04b
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 16:42:15 -0500

    sumatoria: avance 2

--- sumatoria.cpp
+++ sumatoria.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main(){
-    
+    int f,c;
+    int suma[100][100];
+    cin>>f;
+    cin>>c;
+    for(int i=0; i<f; i++){
+        for(int j=0; j<c; j++){
+            cin>>suma[i][j];
+        }
+    }
+
     return 0;
 }
