d34a8c98cf9da355603eb437dae41b5aab80d6f7
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 17:54:26 -0500

    sumadiagonal: terminado

--- sumadiagonal.cpp
+++ sumadiagonal.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
     int n,sum=0;
     int matriz[100][100];
+    cin>>n;
 
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
@@ -11,5 +12,9 @@ int main(){
         }
     }
 
+    for(int i=0; i<n; i++){
+        sum=sum+matriz[i][i];
+    }
+    cout<<sum;
     return 0;
 }
