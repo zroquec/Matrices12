0c302a41b285a7101fa37998eea5af35caa7ef3a
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 17:35:14 -0500

    mayor: terminado

--- Mayor.cpp
+++ Mayor.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    int n,m,mayor;
+    int n,m,mayor,f=1,c=1;
     int matriz[100][100];
     cin>>n;
     cin>>m;
@@ -12,6 +12,17 @@ int main(){
             cin>>matriz[i][j];
         }
     }
-
+    mayor=matriz[0][0];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(mayor<matriz[i][j]){
+                mayor=matriz[i][j];
+                f=i+1;
+                c=j+1;
+            }
+        }
+    }
+    cout<<"Mayor elemento es: "<<mayor<<endl;
+    cout<<"fila: "<<f<<"columna: "<<c;
     return 0;
 }
