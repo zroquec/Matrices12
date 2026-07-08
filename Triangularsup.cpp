cd50d7f4e8c234f1d58f441dc403f88a54638d0f
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 16:28:34 -0500

    Triangularsup: avance 2

--- Triangularsup.cpp
+++ Triangularsup.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
+    int n;
+    cin>>n;
+    int tri[n][n];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i<=j){
+                tri[i][j]=1;
+            }
+            else{
+                tri[i][j]=0;
+            }
+        }
+    }
+    
     return 0;
 }
