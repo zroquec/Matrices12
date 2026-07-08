a536896c9e60be26e20da4fb6cbbdf91c0a06565
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 18:28:47 -0500

    sumaMatrices: terminado

--- sumaMatrices.cpp
+++ sumaMatrices.cpp
@@ -22,5 +22,18 @@ int main(){
         }
     }
 
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            C[i][j]=A[i][j]+B[i][j];
+        }
+    }
+
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            cout<<C[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
