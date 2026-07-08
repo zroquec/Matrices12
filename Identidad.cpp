b2a82ec77b9452a13f1cdad55a1ae4571bde5113
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 09:50:04 -0500

    Identidad: avance 2

--- Identidad.cpp
+++ Identidad.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    int Id[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                Id[i][j] = 1;
+            }
+            else
+            {
+                Id[i][j] = 0;
+            }
+        }
+    }
     return 0;
 }
