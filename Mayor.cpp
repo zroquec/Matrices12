af4affd6119adf507a5a35a81b8a7bf039a97fd8
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 17:08:12 -0500

    mayor: avance 2

--- Mayor.cpp
+++ Mayor.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main(){
+    int n,m,mayor;
+    int matriz[100][100];
+    cin>>n;
+    cin>>m;
     
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin>>matriz[i][j];
+        }
+    }
+
     return 0;
 }
