5e630e7ac3973cf1011b38012a9addfdc0fd1b40
zroquec <zroquec@unjbg.edu.pe>
2026-07-08 19:45:36 -0500

    productoMatices: avance 2

--- productoMatrices.cpp
+++ productoMatrices.cpp
@@ -2,7 +2,19 @@
 using namespace std;
 
 int main(){
-
+    int m,n,p;
+    int A[100][100];
+    int B[100][100];
+    int C[100][100];
+    cin>>m;
+    cin>>n;
+    cin>>p;
     
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+          cin>>A[i][j];  
+        }
+    }
+
     return 0;
 }
